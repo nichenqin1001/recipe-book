f4a6ae20f5006fe30d0c8fe778d160ce7a3e34fc
nichenqin1001 <nichenqin@hotmail.com>
2017-03-13 13:42:14 +0800

    loop recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,10 +8,13 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('link', 'zelda',
+      'http://vignette2.wikia.nocookie.net/zelda/images/d/d0/Princesse_Zelda_Artwork_TPHD.png/revision/latest?cb=20160214225828&path-prefix=fr', []),
+    new Recipe('link', 'zelda',
+      'http://vignette4.wikia.nocookie.net/zelda/images/e/e3/Zelda_Baguette_du_Vent_HW.png/revision/latest?cb=20150201125215&path-prefix=fr', []),
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('link', 'zelda',
-    'http://piq.codeus.net/static/media/forum_pictures/Link_(Spirit_Tracks)_2_2517.png');
 
   constructor() { }
 
